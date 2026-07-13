d83f2899a7de39545a372b95143380c225220b03
Marcos Kohler <marcos_kohler_11@hotmail.com>
2026-07-13 15:39:19 -0300

    fix: impede dano ao próprio jogador

--- Assets/Scripts/Weapons/WeaponAim.cs
+++ Assets/Scripts/Weapons/WeaponAim.cs
@@ -20,6 +20,6 @@ public class WeaponAim : MonoBehaviour
 
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
 
-        transform.rotation = Quaternion.Euler(0f, 0f, angle - 90f);
+        transform.rotation = Quaternion.Euler(0f, 0f, angle + 90f);
     }
 }
